5d387b4d709b9697dd8ddaa77ed94ebc60f58e67
Samuel <sam6camargo@gmail.com>
2024-03-20 10:58:08 -0300

    initial commit

---
+++ src/main/java/com/sam/testealura/TestealuraApplication.java
@@ -0,0 +1,13 @@
+package com.sam.testealura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestealuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestealuraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sam/testealura/TestealuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.testealura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestealuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
