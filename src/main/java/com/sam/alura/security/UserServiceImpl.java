097c4b3b8806178722189c8d55528251bc65570f
Samuel <sam6camargo@gmail.com>
2024-04-19 09:13:02 -0300

    refactor: renamed ApplcationUser to User

--- src/main/java/com/sam/alura/security/UserServiceImpl.java
+++ src/main/java/com/sam/alura/security/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package com.sam.alura.security;
 
-import com.sam.alura.user.ApplicationUser;
+import com.sam.alura.user.User;
 import com.sam.alura.user.CustomUserDetails;
 import com.sam.alura.user.UserRepository;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,7 +18,7 @@ public class UserServiceImpl implements UserDetailsService {
 
     @Override
     public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        ApplicationUser user = userRepository.findUserByUsername(username).orElseThrow(() -> new UsernameNotFoundException("user not found"));
+        User user = userRepository.findUserByUsername(username).orElseThrow(() -> new UsernameNotFoundException("user not found"));
         return new CustomUserDetails(user);
     }
 }
